b7a90d1bbaa95c0be2b663af78770d1b3c8ebef1
Amine LAHRICHI <amine.lahrichi@dauphine.eu>
2019-11-14 11:58:58 +0100

    Auth commenting routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,11 @@
 const express = require("express");
 const router = new express.Router();
+
+//requiring Models
+const userModel = require("./../models/User");
+
+//Sign up
+router.post("/signup", (req, res) => {
+    userModel.create().then().catch()
+  });
 module.exports = router;
