cbe7add05b1ed1c8ca83e33a3285c362acee2b22
Camilosorio <tech@camilo.fr>
2019-11-14 15:22:15 +0100

    Adding the dashboard route

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,4 +1,16 @@
 const express = require("express"); // import express in this module
 const router = new express.Router(); // create an app sub-module (router)
 
+
+//------------------------------------------
+// IMPORT MODELS
+// -----------------------------------------
+const sneakerModel = require("./../models/Sneaker");
+const tagModel = require("./../models/Tag");
+// -----------------------------------------
+
+router.get("/", (req, res) => {
+    res.render("index");
+  });
+
 module.exports = router;
